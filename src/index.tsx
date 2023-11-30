57afec4ddd9c47bd09c48a495af84c41d044c673
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2023-11-29 21:22:26 -0600

    Install bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
+//
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css'
 import './react/index.css';
 import App from './react/App';
 import reportWebVitals from './react/reportWebVitals';
@@ -7,13 +10,11 @@ import reportWebVitals from './react/reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
