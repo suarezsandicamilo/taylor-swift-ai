072682d42053a6b7b44bcd924e0ad0b441279d93
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2023-11-29 20:47:18 -0600

    Separate the express and react code

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './react/index.css';
+import App from './react/App';
+import reportWebVitals from './react/reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
